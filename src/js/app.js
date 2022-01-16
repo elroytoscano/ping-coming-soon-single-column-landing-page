86aaa3127d321a5f6095a24768476e11ebc6dc17
Elroy Toscano <andeltos@gmail.com>
2022-01-16 10:21:11 +0530

    Improved regex, added missing styles

--- src/js/app.js
+++ src/js/app.js
@@ -4,17 +4,20 @@ const inputField = document.querySelector('#emailField');
 const invalidEmailMessage = document.querySelector('#invalid-email');
 
 let isValid = true;
-let regex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
 notifyBtn.addEventListener('click', (e) => {
   e.preventDefault();
   if (inputField.value) {
     isValid = regex.test(inputField.value);
     if (!isValid) {
+      formDiv.classList.remove('gap-y-4');
+      formDiv.classList.add('gap-y-12');
       invalidEmailMessage.classList.remove('invisible');
       inputField.classList.add('border-fmSecondaryLightRed');
     }
   } else {
+    formDiv.classList.remove('gap-y-4');
     invalidEmailMessage.classList.remove('invisible');
     inputField.classList.add('border-fmSecondaryLightRed');
     formDiv.classList.add('gap-y-12');
@@ -22,6 +25,7 @@ notifyBtn.addEventListener('click', (e) => {
 });
 
 inputField.addEventListener('input', () => {
+  formDiv.classList.remove('gap-y-12');
   invalidEmailMessage.classList.add('invisible');
   inputField.classList.remove('border-fmSecondaryLightRed');
   formDiv.classList.add('gap-y-4');
